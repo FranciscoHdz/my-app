0908719ea9476c51f60752b014c215a5c19f6a70
Francisco Daniel Hernández Rodríguez <fco.hernandez90@gmail.com>
2022-01-28 00:25:27 -0600

    Angular Practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' ,component:PresentationComponent},
+  { path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
